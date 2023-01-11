372321811f13f8e8bb885394c64181451e2c84de
mthsena <mthsena@gmail.com>
2023-01-11 08:18:58 -0300

    change app title

--- lib/env/env_app.dart
+++ lib/env/env_app.dart
@@ -1,3 +1,3 @@
 abstract class EnvApp {
-  static const title = String.fromEnvironment('APP_TITLE', defaultValue: 'Desafio Flutter');
+  static const title = String.fromEnvironment('APP_TITLE', defaultValue: 'Github Top 10 Repo');
 }
