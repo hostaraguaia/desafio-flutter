907fb6a85f6cce29c3c96be607b04b264ab89f6b
mthsena <mthsena@gmail.com>
2023-01-11 22:27:27 -0300

    fix a hive issue on convert data

--- lib/cache/cache_service_hive.dart
+++ lib/cache/cache_service_hive.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'cache_service.dart';
@@ -5,13 +7,16 @@ import 'cache_service.dart';
 class CacheServiceHive implements CacheService {
   @override
   Future<void> create(String key, List<Map<String, dynamic>> data) async {
-    final cacheBox = await Hive.openBox<List<Map<String, dynamic>>>('cacheBox');
-    await cacheBox.put(key, data);
+    final cacheBox = await Hive.openBox('cacheBox');
+    final cacheKey = base64.encode(key.codeUnits);
+    cacheBox.put(cacheKey, data);
   }
 
   @override
   Future<List<Map<String, dynamic>>> read(String key) async {
-    final cacheBox = await Hive.openBox<List<Map<String, dynamic>>>('cacheBox');
-    return cacheBox.get(key) ?? [];
+    final cacheBox = await Hive.openBox('cacheBox');
+    final cacheKey = base64.encode(key.codeUnits);
+    final data = json.encode(cacheBox.get(cacheKey) ?? []);
+    return List<Map<String, dynamic>>.from(json.decode(data));
   }
 }
