50f45cab1fc7cb120c413a69712b8358a4a21acc
mthsena <mthsena@gmail.com>
2023-01-12 20:32:35 -0300

    add github user tests

---
+++ test/github/user/github_user_model_test.dart
@@ -0,0 +1,25 @@
+import 'package:desafio_flutter/github/user/github_user_entity.dart';
+import 'package:desafio_flutter/github/user/github_user_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late GithubUserEntity entity;
+  late GithubUserEntity emptyEntity;
+
+  setUpAll(() {
+    entity = const GithubUserEntity(id: 10, username: 'user', type: 'org', avatarUrl: 'https://www.google.com/profile.jpg');
+    emptyEntity = const GithubUserEntity(id: 0, username: '', type: '', avatarUrl: '');
+  });
+
+  test('should parse entity to model', () async {
+    final model = GithubUserModel.fromEntity(entity);
+    expect(model, isA<GithubUserModel>());
+    expect(model.username, equals('user'));
+  });
+
+  test('should parse empty entity to model', () async {
+    final model = GithubUserModel.fromEntity(emptyEntity);
+    expect(model, isA<GithubUserModel>());
+    expect(model.username, equals(''));
+  });
+}
