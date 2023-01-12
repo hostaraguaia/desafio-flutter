20ad254f5d6894ab2763eeea76c8f63fd5389eeb
mthsena <mthsena@gmail.com>
2023-01-11 23:10:29 -0300

    add get_it as dependency injection

---
+++ lib/injection.dart
@@ -0,0 +1,28 @@
+import 'package:get_it/get_it.dart';
+
+import 'cache/cache_service_hive.dart';
+import 'github/pull/github_pull_repository_default.dart';
+import 'github/pull/list/github_pull_list_cubit.dart';
+import 'github/pull/list/github_pull_list_usecase_default.dart';
+import 'github/repo/github_repo_repository_default.dart';
+import 'github/repo/list/github_repo_list_cubit.dart';
+import 'github/repo/list/github_repo_list_usecase_default.dart';
+
+final getIt = GetIt.instance;
+
+void setupInjection() {
+  // Services
+  getIt.registerSingleton(CacheServiceHive());
+
+  // Repositories
+  getIt.registerSingleton(GithubRepoRepositoryDefault(getIt.get<CacheServiceHive>()));
+  getIt.registerSingleton(GithubPullRepositoryDefault(getIt.get<CacheServiceHive>()));
+
+  // Usecases
+  getIt.registerSingleton(GithubRepoListUsecaseDefault(getIt.get<GithubRepoRepositoryDefault>()));
+  getIt.registerSingleton(GithubPullListUsecaseDefault(getIt.get<GithubPullRepositoryDefault>()));
+
+  // Cubits
+  getIt.registerSingleton(GithubRepoListCubit(getIt.get<GithubRepoListUsecaseDefault>()));
+  getIt.registerSingleton(GithubPullListCubit(getIt.get<GithubPullListUsecaseDefault>()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-import 'cache/cache_service_hive.dart';
 import 'env/env_app.dart';
-import 'github/pull/github_pull_repository_default.dart';
 import 'github/pull/list/github_pull_list_cubit.dart';
-import 'github/pull/list/github_pull_list_usecase_default.dart';
 import 'github/repo/github_repo_page.dart';
-import 'github/repo/github_repo_repository_default.dart';
 import 'github/repo/list/github_repo_list_cubit.dart';
-import 'github/repo/list/github_repo_list_usecase_default.dart';
+import 'injection.dart';
 import 'main/main_widget.dart';
 import 'theme/theme_app.dart';
 
 void main() async {
+  setupInjection();
+
   await Hive.initFlutter();
   await Hive.deleteFromDisk();
 
@@ -22,20 +20,8 @@ void main() async {
       title: EnvApp.title,
       theme: ThemeApp.data,
       child: GithubRepoPage(
-        GithubRepoListCubit(
-          GithubRepoListUsecaseDefault(
-            GithubRepoRepositoryDefault(
-              CacheServiceHive(),
-            ),
-          ),
-        ),
-        GithubPullListCubit(
-          GithubPullListUsecaseDefault(
-            GithubPullRepositoryDefault(
-              CacheServiceHive(),
-            ),
-          ),
-        ),
+        getIt.get<GithubRepoListCubit>(),
+        getIt.get<GithubPullListCubit>(),
         title: EnvApp.title,
       ),
     ),
