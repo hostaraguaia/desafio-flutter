342a9155dfbc29aa78f5474189ffcdb24a3b6df6
mthsena <mthsena@gmail.com>
2023-01-13 17:09:39 -0300

    add api service tests and handle errors

--- lib/api/api_service_github.dart
+++ lib/api/api_service_github.dart
@@ -3,20 +3,25 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 
 import 'api_service.dart';
+import 'api_service_error.dart';
 
 class ApiServiceGithub extends ApiService {
   final url = 'https://api.github.com';
 
   @override
   Future<dynamic> request(String method, String path, Map<String, dynamic> data, Map<String, dynamic> header) async {
-    final dio = Dio();
-    final response = await dio.request(
-      url + path,
-      options: Options(
-        method: method.toUpperCase(),
-        headers: header,
-      ),
-    );
-    return json.decode(json.encode(response.data));
+    try {
+      final dio = Dio();
+      final response = await dio.request(
+        url + path,
+        options: Options(
+          method: method.toUpperCase(),
+          headers: header,
+        ),
+      );
+      return json.decode(json.encode(response.data));
+    } on DioError catch (e) {
+      throw ApiServiceError(e.message);
+    }
   }
 }
